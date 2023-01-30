d5525b2c43a4d62c397ef940ff3eda89c20fd6e4
yeaahYEAHH <81806907+yeaahYEAHH@users.noreply.github.com>
2023-01-30 10:54:51 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,80 @@
+window.addEventListener("DOMContentLoaded", () => {
+    
+    //tabs
+    const tabs = document.querySelectorAll('.tabheader__item'), 
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsPatern = document.querySelector('.tabheader');
+
+    function hideTabContent(){
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        })
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active')
+        })
+    }
+
+    function showTabContent(i = 0){
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+    hideTabContent();
+    showTabContent();
+
+    tabsPatern.addEventListener('click', (event) => {
+        const target = event.target;
+        if(target && target.classList.contains('tabheader__item')){
+            tabs.forEach((item, i) =>{
+                if(target == item){
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    })
+
+    //timer
+    const timeEnd = '2021-11-20';
+    function getTimeRemainig(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t/ (1000*60*60*24)),
+            hours = Math.floor(t / (1000 * 60 * 60) % 24),
+            minutes = Math.floor((t / 1000 / 60 ) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        return{
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector),
+            days = document.querySelector('#days'),
+            hours = document.querySelector('#hours'),
+            minutes = document.querySelector('#minutes'),
+            seconds = document.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock()
+
+        function updateClock(){
+            const t = getTimeRemainig(endtime);
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+
+        }
+    }
+
+    setClock('.timer', timeEnd)
+
+
+
+    //modal 
+})
+
